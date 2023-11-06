da01fe2ee4c4d2908a242f0eda01cea7d281b7a5
dh0321 <111106711+dh0321@users.noreply.github.com>
2023-11-06 00:39:10 +0000

    Create increasing_vacteria.py
    프로그래머스 - 세균 증식(python)

---
+++ python/increasing_vacteria.py
@@ -0,0 +1,25 @@
+# 세균 증식
+
+'''
+Answer 1.
+
+def solution(n, t):
+    for i in range(t) :
+        n *= 2
+    return n
+'''
+
+'''
+Answer 2.
+
+def solution(n, t):
+    answer = n * 2**t
+    return answer
+'''
+
+'''
+Answer 3.
+
+def solution(n, t):
+    return n << t
+'''
