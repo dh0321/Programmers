4dd7cbfdae410dd76ff136c8b83733aa79b6ed7c
dh0321 <111106711+dh0321@users.noreply.github.com>
2023-11-07 02:29:22 +0000

    Update string_to_int.cpp
    프로그래머스 - 문자열을 정수로 바꾸기(C++)

--- cpp/lv1/string_to_int.cpp
+++ cpp/lv1/string_to_int.cpp
@@ -1,3 +1,37 @@
 // 문자열을 정수로 바꾸기
 
-test file
+/* Answer 1
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = stoi(s);   
+    
+    return answer;
+}
+
+stoi = sring to integer
+C++11 이후로 사용. */
+
+
+/* Answer 2
+
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    stringstream ss;
+    ss.str(s);
+    ss >> answer;
+
+    return answer;
+}
+
+C++11 이전 버전. */
