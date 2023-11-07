2066ed7bbe2879ab9d6a89ff37d0034afcd23884
dh0321 <111106711+dh0321@users.noreply.github.com>
2023-11-07 01:51:48 +0000

    Create even_and_odd.cpp
    프로그래머스 - 짝수와 홀수(C++)

---
+++ cpp/lv1/even_and_odd.cpp
@@ -0,0 +1,26 @@
+// 짝수와 홀수
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/* Answer 1
+
+  string solution(int num) {
+    string answer = "";
+    
+    if(num % 2)
+    {
+        answer = "Odd";
+    }
+    else answer = "Even";
+    
+    
+    return answer;
+} */
+
+/* Answer 2
+
+string solution(int num) {
+    return num % 2 ? "Odd" : "Even";
+} */
