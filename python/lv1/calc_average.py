1874b26217ff7ae27a2b8362669c6af0260895a0
dh0321 <111106711+dh0321@users.noreply.github.com>
2023-11-07 01:29:55 +0000

    Create calc_average.py
    프로그래머스 - 평균 구하기(python)

---
+++ python/lv1/calc_average.py
@@ -0,0 +1,23 @@
+# 평균 구하기
+
+'''
+Answer 1.
+
+def solution(arr):
+    sum_arr = 0
+    avg = 0
+    
+    for i in arr:
+        sum_arr += i
+        
+    avg = sum_arr / len(arr)
+    
+    return avg
+'''
+
+'''
+Answer 2.
+
+def solution(arr):
+    return sum(arr) / len(arr)
+'''
