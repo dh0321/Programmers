93b4a226895501e47e2fc0ba785720096a910a8e
dh0321 <111106711+dh0321@users.noreply.github.com>
2023-11-07 01:23:51 +0000

    Create even_and_odd.py
    프로그래머스 - 짝수와 홀수(python)

---
+++ python/lv1/even_and_odd.py
@@ -0,0 +1,34 @@
+# 짝수와 홀수
+
+'''
+Answer 1.
+
+def solution(num):
+    answer = ''
+    
+    if num % 2 == 0:
+        answer = 'Even'
+    else:
+        answer = 'Odd'
+    
+    return answer
+
+'''
+
+'''
+Answer 2.
+
+def solution(num):
+    if num%2:
+        return "Odd"
+
+    return "Even"
+
+'''
+
+'''
+Answer 3.
+
+def evenOrOdd(num):
+    return "Even" if num % 2 == 0 else "Odd"
+'''
